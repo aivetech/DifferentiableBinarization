06a14b88689ff96dad9be0965ed29a00a46eef10
duforetn <nicolas@aive.com>
2020-04-20 08:09:32 +0000

    use tensorflow-gpu

--- train_simpler.py
+++ train_simpler.py
@@ -11,29 +11,29 @@ from model import dbnet
 from model_simpler import db_simpler
 checkpoints_dir = f'checkpoints/{datetime.date.today()}'
 
-batch_size = 2
+batch_size = 8
 
 if not osp.exists(checkpoints_dir):
     os.makedirs(checkpoints_dir)
 
-train_generator = generate_simpler_model('datasets/total_text_subsample', batch_size=batch_size, is_training=True)
-val_generator = generate_simpler_model('datasets/total_text_subsample', batch_size=batch_size, is_training=False)
+train_generator = generate_simpler_model('dataset/dataset', batch_size=batch_size, is_training=True)
+val_generator = generate_simpler_model('dataset/dataset', batch_size=batch_size, is_training=False)
 
 model, prediction_model = db_simpler()
 
-model.compile(optimizer=optimizers.Adam(lr=1e-3), loss={'conv2d_transpose_1': lambda y_true, y_pred: simpler_loss(y_pred, y_true)})
+model.compile(optimizer=optimizers.Adam(lr=1e-4), loss={'conv2d_proba_2': lambda y_true, y_pred: simpler_loss(y_pred, y_true)})
 checkpoint = callbacks.ModelCheckpoint(
-    osp.join(checkpoints_dir, 'simpler_{epoch:02d}_{loss:.4f}_{val_loss:.4f}.h5'),
+    osp.join(checkpoints_dir, 'simpler_incrust_text_{epoch:02d}_{loss:.4f}_{val_loss:.4f}.h5'),
     verbose=1,
 )
 model.fit(
     x=train_generator,
-    steps_per_epoch=2,
+    steps_per_epoch=100,
     initial_epoch=0,
-    epochs=1,
+    epochs=100,
     verbose=1,
     callbacks=[checkpoint],
     validation_data=val_generator,
-    validation_steps=2
+    validation_steps=8
 )
 
