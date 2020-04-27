bd2b80d7a0a4896ce4b367cb12919038c226342d
Nicolas Duforet <nicolas@aive.com>
2020-04-27 12:13:17 +0200

    simple generator with gt_masks only

--- simpler_generator.py
+++ simpler_generator.py
@@ -37,15 +37,6 @@ def load_all_anns(gt_paths, dataset='total_text'):
     return res
 
 
-def show_polys(image, anns, window_name):
-    for ann in anns:
-        poly = np.array(ann['poly']).astype(np.int32)
-        cv2.drawContours(image, np.expand_dims(poly, axis=0), -1, (0, 255, 0), 2)
-
-    cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
-    cv2.imshow(window_name, image)
-
-
 def compute_distance(xs, ys, point_1, point_2):
     square_distance_1 = np.square(xs - point_1[0]) + np.square(ys - point_1[1])
     square_distance_2 = np.square(xs - point_2[0]) + np.square(ys - point_2[1])
@@ -67,8 +58,7 @@ def generate_simpler_model(data_dir, batch_size=16, image_size=640, min_text_siz
     with open(osp.join(data_dir, f'{split}_list.txt')) as f:
         image_fnames = f.readlines()
         image_paths = [osp.join(data_dir, f'{split}_images', image_fname.strip()) for image_fname in image_fnames]
-        gt_paths = [osp.join(data_dir, f'{split}_gts', image_fname.strip() + '.txt') for image_fname in image_fnames]
-        all_anns = load_all_anns(gt_paths)
+        gt_mask_paths = [osp.join(data_dir, f'{split}_gt_masks', image_fname.strip() ) for image_fname in image_fnames]
     transform_aug = iaa.Sequential([iaa.Fliplr(0.5), iaa.Affine(rotate=(-10, 10)), iaa.Resize((0.5, 3.0))])
     dataset_size = len(image_paths)
     indices = np.arange(dataset_size)
@@ -78,8 +68,6 @@ def generate_simpler_model(data_dir, batch_size=16, image_size=640, min_text_siz
     b = 0
     while True:
         if current_idx >= dataset_size:
-            if is_training:
-                np.random.shuffle(indices)
             current_idx = 0
         if b == 0:
             # Init batch arrays
@@ -87,53 +75,40 @@ def generate_simpler_model(data_dir, batch_size=16, image_size=640, min_text_siz
             batch_gts = np.zeros([batch_size, image_size, image_size], dtype=np.float32)
         i = indices[current_idx]
         image_path = image_paths[i]
-        anns = all_anns[i]
         image = cv2.imread(image_path)
-        # show_polys(image.copy(), anns, 'before_aug')
-        if is_training:
-            transform_aug = transform_aug.to_deterministic()
-            image, anns = transform(transform_aug, image, anns)
-            image, anns = crop(image, anns)
-        image, anns = resize(image_size, image, anns)
-        # show_polys(image.copy(), anns, 'after_aug')
-        # cv2.waitKey(0)
-        anns = [ann for ann in anns if Polygon(ann['poly']).is_valid]
+        #if is_training:
+        #    transform_aug = transform_aug.to_deterministic()
+        #    image, anns = transform(transform_aug, image, anns)
+        #    image, anns = crop(image, anns)
+        h, w, c = image.shape
+        scale_w = image_size / w
+        scale_h = image_size / h
+        scale = min(scale_w, scale_h)
+        h = int(h * scale)
+        w = int(w * scale)
+        padimg = np.zeros((image_size, image_size, c), image.dtype)
+        padimg[:h, :w] = cv2.resize(image, (w, h))
+        image = padimg
         gt = np.zeros((image_size, image_size), dtype=np.float32)
-        for ann in anns:
-            poly = np.array(ann['poly'])
-            height = max(poly[:, 1]) - min(poly[:, 1])
-            width = max(poly[:, 0]) - min(poly[:, 0])
-            polygon = Polygon(poly)
-            # generate gt and mask
-            if polygon.area < 1 or min(height, width) < min_text_size or ann['text'] == '###':
-                continue
-            else:
-                distance = polygon.area * (1 - np.power(shrink_ratio, 2)) / polygon.length
-                subject = [tuple(l) for l in ann['poly']]
-                padding = pyclipper.PyclipperOffset()
-                padding.AddPath(subject, pyclipper.JT_ROUND, pyclipper.ET_CLOSEDPOLYGON)
-                shrinked = padding.Execute(-distance)
-                if len(shrinked) == 0:
-                    continue
-                else:
-                    shrinked = np.array(shrinked[0]).reshape(-1, 2)
-                    if shrinked.shape[0] > 2 and Polygon(shrinked).is_valid:
-                        cv2.fillPoly(gt, [shrinked.astype(np.int32)], 1)
-                    else:
-                        continue
+        gt = cv2.imread(gt_mask_paths[current_idx], 0)
+        padgt = np.zeros((image_size, image_size), image.dtype)
+        padgt[:h, :w] = cv2.resize(gt, (w, h))
+        gt = np.round(padgt/255)
         
         image = image.astype(np.float32)
         image[..., 0] -= mean[0]
         image[..., 1] -= mean[1]
         image[..., 2] -= mean[2]
         batch_images[b] = image
+        
         batch_gts[b] = gt
-        print(gt.shape)
+        
         b += 1
         current_idx += 1
         if b == batch_size:
-            #inputs = [batch_images, batch_gts, batch_masks, batch_thresh_maps, batch_thresh_masks]
             inputs = batch_images
             outputs = batch_gts
             yield inputs, outputs
             b = 0
+
+
